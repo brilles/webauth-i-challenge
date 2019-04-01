043d858100906593bb12c95b1a8475a4b22695b8
Brian Illes <brianrilles@gmail.com>
2019-04-01 15:51:57 -0700

    Refactor

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,17 +1,34 @@
 const faker = require('faker');
 const bcrypt = require('bcryptjs');
 
-function generateRandomCredentials(amount) {
+// function generateRandomCredentials(amount) {
+//   const usersArray = [];
+//   for (let i = 0; i < amount; i++) {
+//     let username = faker.internet.userName();
+//     let password = bcrypt.hashSync(faker.internet.password(), 8);
+
+//     usersArray.push({ username, password });
+//   }
+//   return usersArray;
+// }
+
+function creds(amount) {
+  function* credGenerator() {
+    yield {
+      username: faker.internet.userName(),
+      password: bcrypt.hashSync(faker.internet.password(), 8)
+    };
+  }
+
   const usersArray = [];
   for (let i = 0; i < amount; i++) {
-    let username = faker.internet.userName();
-    let password = bcrypt.hashSync(faker.internet.password(), 8);
-
-    usersArray.push({ username, password });
+    usersArray.push(credGenerator().next().value);
   }
   return usersArray;
 }
 
+console.log(creds(100));
+
 exports.seed = function(knex) {
-  return knex('users').insert(generateRandomCredentials(100));
+  return knex('users').insert(creds(100));
 };
